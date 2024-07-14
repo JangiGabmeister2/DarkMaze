fada7f6232ae9736357bc2336f9aaf6b8701073b
JangiGabmeister2 <109714078+JangiGabmeister2@users.noreply.github.com>
2024-07-14 11:16:10 +1000

    added inverted fog of war sprite
    edited level 5 scene to experiment with new fog of war effect

--- Assets/Scripts/ExpandMask.cs
+++ Assets/Scripts/ExpandMask.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public class ExpandMask : MonoBehaviour
 {
     [SerializeField] private Transform _spriteMask;
+    [SerializeField] private float _minScale = 5f, _maxScale = 100f;
 
     private Vector3 _speed = new Vector3(100, 100, 100);
 
@@ -19,7 +20,7 @@ public class ExpandMask : MonoBehaviour
 
     private IEnumerator Expand()
     {
-        while (_spriteMask.localScale.y < 100)
+        while (_spriteMask.localScale.y < _maxScale)
         {
             _spriteMask.localScale += _speed * Time.deltaTime;
 
@@ -29,7 +30,7 @@ public class ExpandMask : MonoBehaviour
 
     private IEnumerator Shrink()
     {
-        while (_spriteMask.localScale.y > .1f)
+        while (_spriteMask.localScale.y > _minScale)
         {
             _spriteMask.localScale -= _speed * Time.deltaTime;
 
